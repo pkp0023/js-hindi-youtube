d142a71eee21d5e190f6cfe67f1821ce4d04ddfe
PRINCE KUMAR PANDEY <136254118+pkp0023@users.noreply.github.com>
2026-02-28 09:24:44 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "hitesh"
+// in case of null it is showing 0
+// // in case of true it is showing 1
+// // in case of false it is showing 0
+// // In case of string it is showing Nan , because it is not a number 
+
+// console.log(typeof score);
+// let valueInNumber = Number(score) // Don't rely on this because sometime we don't know do variable contains exactly number
+// console.log(valueInNumber);
+
+
+let isLoggedIn = false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
