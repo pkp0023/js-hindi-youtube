741ad83667982606924151db416024453fcb1e46
PRINCE KUMAR PANDEY <136254118+pkp0023@users.noreply.github.com>
2026-02-28 10:27:37 +0000

    Learn about some conversion operation, also know about prefix and suffix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,15 +9,53 @@
 // console.log(valueInNumber);
 
 
-let isLoggedIn = false
+// let isLoggedIn = false
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+
+//  ******************** Operations ********************
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "hello"
+let str2 = "pandey"
+
+let str3 = str1+str2
+console.log(str3)
+
+// console.log("1"+ "2");
+// console.log(1+"2"+1) //outpur 121
+// console.log(1+1+"2") //output 22
+// console.log((3+4) * 5 % 3); // always use brackets, because it is a good habit
+
+// console.log(+true); // it gives output 1
+// console.log(+""); //it gives 0
+
+let num1,num2,num3
+
+// num1 = num2 = num3 = 2+2 - Don't do this , this is wrong practise, Although it gives desire output but don't do this
+ let gamecounter = 100
+ 
+ ++gamecounter;
+ ++gamecounter;
+ gamecounter++;
+ console.log(gamecounter);
+
+ let y = 6
+ console.log(++y);
+ console.log(y)
+
+
+ let x = 5;
+console.log(x++); // 5
+console.log(x);   // 6
